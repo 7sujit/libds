92546337930cfc14ac017bac844701820e66f2a6
Sujit Kumar Muduli <muduli_sk@yahoo.com>
2015-08-21 13:24:36 +0530

    Update segtree.h

--- Segment-Tree-LinkedList/segtree.h
+++ Segment-Tree-LinkedList/segtree.h
@@ -16,9 +16,16 @@ struct SegTreeNode
     struct SegTreeNode *left_child,*right_child;
 };
 
+/*
+    'build_seg_tree' function takes three arguements 
+    1-> array of type int as first arguement
+    2-> first index of the array
+    3-> last index of the array
+    
+    after creating the segment tree 'build_seg_tree' returns the address of the root noe
+*/
+
 struct SegTreeNode* build_seg_tree(int *input,int low, int high);
 void get_max_and_freq(struct SegTreeNode* root, int q_left, int q_right, int *store_max, int *store_freq);
-// store_max is a pointer to the variable where we will be storing the max value
-// store_freq is a pointer to the variable where we'll be storing the max value freq
 
 #endif
